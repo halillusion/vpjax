cafc62c960f0acd103111a685d318bdec7519bc8
Halil IO <halillusion@gmail.com>
2022-02-07 00:02:55 +0300

    code ql bug fix for test.

--- test.js
+++ test.js
@@ -1,10 +1,19 @@
 'use strict'
 
 const express = require('express')
+const rateLimit = require('express-rate-limit')
 const path = require('path')
 const app = express()
 const port = 3000
 
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+})
+
+app.use(limiter)
 app.use('/src', express.static(path.join(__dirname)))
 
 app.get('/', function(req, res) {
